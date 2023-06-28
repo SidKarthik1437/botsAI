68a05e07fc442f9c103c0751cda8e95a4f08f41d
Sid Karthik <sa268592@gmail.com>
2023-06-29 00:16:25 +0530

    v1.0

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pages.AssistantX as assistantx
+
+st.title("Welcome to BotsAI")
+st.text("Select a bot from the sidebar to get started!")
